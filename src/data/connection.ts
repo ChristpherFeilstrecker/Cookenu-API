cf0bb2e319d70da8cc46348a401e21f4ff7aae35
Christopher <jokinha95@gmail.com>
2022-02-25 11:25:12 -0300

    original

--- src/data/connection.ts
+++ src/data/connection.ts
@@ -6,7 +6,7 @@ dotenv.config()
 const connection = knex({
     client: 'mysql',
     connection: {
-        host: "35.226.146.116",
+        host: process.env.DB_HOST,
         port: 3306,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
