e5f4135afc96212d21cbecd44d0ea3da5eb2b70a
Christopher <jokinha95@gmail.com>
2022-02-25 11:22:38 -0300

    novo host

--- src/data/connection.ts
+++ src/data/connection.ts
@@ -6,7 +6,7 @@ dotenv.config()
 const connection = knex({
     client: 'mysql',
     connection: {
-        host: 'mysql-docker',
+        host: "35.226.146.116",
         port: 3306,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
