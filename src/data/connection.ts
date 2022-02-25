952fc439f58b6339587811ca54c47ee9a5553c11
Christopher <jokinha95@gmail.com>
2022-02-25 11:18:51 -0300

    erro conecção alterando host

--- src/data/connection.ts
+++ src/data/connection.ts
@@ -6,7 +6,7 @@ dotenv.config()
 const connection = knex({
     client: 'mysql',
     connection: {
-        host: process.env.DB_HOST,
+        host: 'mysql-docker',
         port: 3306,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
